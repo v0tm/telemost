1f4d7e8c1f20e486a20283639173c5cdfaec8f6f
Ilya Mashintsev <ilya.mashintsev@gcore.com>
2023-05-19 13:47:21 +0400

    DB fix (integrity)

--- services/db.py
+++ services/db.py
@@ -1,5 +1,7 @@
+from modules.database import session
 from modules.models import Chat, Message, User
 from telegram import Message as TelegramMessage
+from sqlalchemy.exc import IntegrityError, PendingRollbackError
 
 
 class DatabaseServices:
@@ -12,7 +14,12 @@ class DatabaseServices:
         :param scope: Custom scope (for example for storing command logs).
         :return: None
         """
-        u = message.from_user
-        chat = Chat.get_or_create(id=message.chat.id, name=message.chat.title)
-        user = User.get_or_create(id=u.id, username=u.username, first_name=u.first_name, last_name=u.last_name)
-        Message.create(id=message.id, chat_id=chat.id, user_id=user.id, text=message.text, scope=scope or chat.scope)
+        try:
+            u = message.from_user
+            chat = Chat.get_or_create(id=message.chat.id, name=message.chat.title)
+            user = User.get_or_create(id=u.id, username=u.username, first_name=u.first_name, last_name=u.last_name)
+            Message.create(id=message.id, chat_id=chat.id, user_id=user.id, text=message.text, scope=scope or chat.scope)
+        except (IntegrityError, PendingRollbackError) as ex:
+            error_message = ex.args[0] if len(ex.args) > 0 else ex.__class__.__name__
+            print(f'ERROR WITH SAVING THE MESSAGE, ROLLING BACK... ({error_message})')
+            session.rollback()
